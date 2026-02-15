a4699ef56f6208d6969bbd1740f6cfd41f4cdab7
rianfernandesno <zrpapz12@gmail.com>
2026-02-14 23:01:16 -0300

    command ping -> pong

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,31 @@
-package com.yui.bot
+package com.yui.b
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+suspend fun main(){
+    val token: String = System.getenv("BOT_TOKEN") ?: error("A variável de ambiente 'BOT_TOKEN' não foi encontrada na configuração")
+
+    println("Bot Selene acordando...")
+
+    val kord = Kord(token = token)
+
+    kord.on<MessageCreateEvent>{
+        if(message.author?.isBot != false) return@on
+
+        if(message.content != "!ping") return@on
+
+        message.channel.createMessage("pong")
     }
+
+    kord.login{
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+        println("✅ Bot inicializado com sucesso!")
+    }
+
+
 }
